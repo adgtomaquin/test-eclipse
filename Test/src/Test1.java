072f3b1520cb62f6b4dced52d108e0c3fe8750f1
TOMAQUINAD <TOMAQUINAD@L21-00053953.bsp.gov.ph>
2026-05-13 14:36:12 +0800

    Updated

--- Test/src/Test1.java
+++ Test/src/Test1.java
@@ -5,7 +5,7 @@ import java.nio.charset.StandardCharsets;
 public class Test1 {
 	public static void main(String args[]) {
 		if("true".equals("true"))
-    		System.out.println("Test");
+    		System.out.println("Test2");
 		
 	
 	}
