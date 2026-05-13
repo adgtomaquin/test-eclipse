6c06c50c6eb03de72a9b1e2cc73d820450436e09
TOMAQUINAD <TOMAQUINAD@L21-00053953.bsp.gov.ph>
2026-05-13 13:57:27 +0800

    Initial

---
+++ Test/src/Test1.java
@@ -0,0 +1,12 @@
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class Test1 {
+	public static void main(String args[]) {
+		if("true".equals("true"))
+    		System.out.println("Test");
+		
+	
+	}
+}
